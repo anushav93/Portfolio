f9b71ed98801ec6efa2074ddf6b19b8875b51dc5
anushav <anushaventrapragada93@gmail.com>
2018-10-28 14:51:28 -0500

    Contact form using netlify

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 
+
+
 const Contact = () => {
+
+
   return (
     <React.Fragment>
       <div className="container page-content">
         <div className="row">
         <div className="col-12">
-        <h2>
-        <b>R</b>
-        each me at anushaventrapragada93@gmail.com
-      </h2>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
         </div>
         </div>
       
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,9 @@ const Footer = () => {
     <div className="container footer mt-4">
       <div className="row">
         <div className="col-8 text-left">Anusha | Vdesign | 2018</div>
-        <div className="col-4 text-right"><a href="https://github.com/anushav93" target="_blank" className="fa fa-github-alt mx-3"/><a href="" className="fa fa-linkedin" /></div>
+        <div className="col-4 text-right">
+        <a href="https://github.com/anushav93" target="_blank" className="fa fa-github-alt mx-3"/>
+        <a href="" className="fa fa-linkedin" /></div>
       </div>
     </div>
   );
