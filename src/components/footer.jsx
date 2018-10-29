0e0cd59db87ceddcf6a63c410462c2abaa4c581d
anushav9 <anushaventrapragada93@gmail.com>
2018-10-28 22:00:59 -0500

    form submition success page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Icon from '@mdi/react';
 import { mdiGmail, mdiLinkedin,mdiGithubFace} from '@mdi/js';
 
---
+++ src/components/success.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Success = () => {
+  return (
+      <React.Fragment>
+          <div className="container">
+              <h2>Form Submitted Successfully !!</h2>
+          </div>
+      </React.Fragment>
+
+  );
+};
+
+export default Success;
